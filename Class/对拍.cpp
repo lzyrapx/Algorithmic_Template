434db1f2466179618759e08e11aec955f275945d
LzyRapx <1303377506@qq.com>
2018-04-02 12:34:43 +0800

    Update 对拍.cpp

--- Class/对拍.cpp
+++ Class/对拍.cpp
@@ -15,3 +15,30 @@ int main()
   
     return 0;
 }
+
+//------------------------------------------------------------------//
+#include<bits/stdc++.h>
+using namespace std;
+#define maxn 1000010
+char ans[maxn][55];
+char ac[maxn][55];
+char s[55];
+int main() {
+    int a=0,b=0;
+    freopen("ac.txt","r",stdin);
+    while(gets(s)) strcpy(ac[a++],s);
+    freopen("ans.txt","r",stdin);
+    while(gets(s)) strcpy(ans[b++],s);
+    if(a!=b) printf("Error!\n");
+    else {
+        int wa=0;
+        for(int i=0; i<a; i++) {
+            if(strcmp(ans[i],ac[i])!=0) {
+                printf("%d : %s / %s\n",i+1,ac[i],ans[i]);
+                wa++;
+            }
+        }
+        printf("wrong answer : %d\n",wa);
+    }
+    return 0;
+}
