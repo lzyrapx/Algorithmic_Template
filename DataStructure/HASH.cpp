5f608b3fc26aca8e09261ddbea80415b0cad6aa8
LzyRapx <1303377506@qq.com>
2018-04-04 22:24:02 +0800

    Update HASH.cpp

--- DataStructure/HASH.cpp
+++ DataStructure/HASH.cpp
@@ -1,18 +1,23 @@
 /*    HASH
-   数字HASH,开散列,邻接表   */
+   数字HASH,开散列,邻接表   
+*/
 
 const int MOD=4001,STA=1000010;  //MOD为表长,STA为表大小
 
-struct Hash{
+struct Hash
+{
     int first[MOD],next[STA],size;
     LL f[STA],sta[STA];   //sta[]存放状态,f[]为对应状态的权值
-    void init(){
+    void init()
+    {
         size=0;
         mem(first,-1);
     }
-    int find_add(LL st,LL ans){   //查找,如果未查找到则添加   
+    int find_add(LL st,LL ans)
+    {   //查找,如果未查找到则添加   
         int i,u=st%MOD;
-        for(i=first[u];i!=-1;i=next[i]){
+        for(i=first[u];i!=-1;i=next[i])
+        {
             if(sta[i]==st){
                 f[i]+=ans;   //状态累加,注意啦
                 return 1;   //已存在状态
