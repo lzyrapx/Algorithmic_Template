3c8962ab11ea8cba60be246e2924be81a5262695
LzyRapx <1303377506@qq.com>
2018-05-03 20:36:24 +0800

    Update Dijkstra(1).cpp

--- Graph-theory/Shortest-path/Dijkstra(1).cpp
+++ Graph-theory/Shortest-path/Dijkstra(1).cpp
@@ -18,32 +18,32 @@ int start,end;
 
 int dijlstra()
 {
-	for(int i=1;i<=end;i++) dist[i]=MaxInt;
+      for(int i=1;i<=end;i++) dist[i]=MaxInt;
       memset(mark,0,sizeof(mark));
       dist[start]=0;
       //把起点并入集合，搜索的就可以从起点寻找第一条最短的边了
-	  for(int i=1;i<=end-1;i++)
+      for(int i=1;i<=end-1;i++)
+      {
+	  min1=MaxInt;
+	  for(int j=1;j<=end;j++)  //查找到原集合的最短的边 
 	  {
-	  	min1=MaxInt;
-	  	for(int j=1;j<=end;j++)  //查找到原集合的最短的边 
+	  	if(!mark[j] && dist[j]<min1)
 	  	{
-	  		if(!mark[j] && dist[j]<min1)
-	  		{
-	  			min1=dist[j];
-	  			minj=j;
-	  		}
+	  		min1=dist[j];
+	  		minj=j;
 	  	}
-	  	mark[minj]=1;
-		//每并入一个点都要对原来的变进行修正，保证任意时刻源点到目标点的距离都是最短的 
-		  for(int j=1;j<=end;j++)
-		  {
-		  	if(!mark[j] && map[minj][j]>0)
-			  {
-			  	temp=dist[minj]+map[minj][j];
-			  	if(temp<dist[j]) dist[j]=temp;
-			  } 
-		  } 
-	  } 
-	  return dist[end];
+	  }
+	  mark[minj]=1;
+	//每并入一个点都要对原来的变进行修正，保证任意时刻源点到目标点的距离都是最短的 
+	for(int j=1;j<=end;j++)
+	{
+		if(!mark[j] && map[minj][j]>0)
+		{
+		    temp=dist[minj]+map[minj][j];
+		    if(temp<dist[j]) dist[j]=temp;
+		} 
+	} 
+      } 
+      return dist[end];
 }
  
