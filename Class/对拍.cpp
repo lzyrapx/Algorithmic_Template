f55170660b2e75d836efeb76925316457832576b
LzyRapx <LzyRapx@gmail.com>
2018-05-22 23:12:47 +0800

    update

--- Class/对拍.cpp
+++ Class/对拍.cpp
@@ -42,3 +42,30 @@ int main() {
     }
     return 0;
 }
+
+//------------------------------------------------------------------//
+#include <bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+int main()
+{
+    system("g++ data.cpp -o data -O2");
+    system("g++ std.cpp -o std -O2");
+    system("g++ my.cpp -o test");
+    clock_t st,ed;
+    for (int i=1;i<=10;i++)
+    {
+        printf("%d\n",i);
+        //Sleep(1000);
+        system("data");
+        system("std");
+        st=clock();
+        system("test");
+        ed=clock();
+        printf("Time used:%lfs\n",(double)(ed-st)/CLOCKS_PER_SEC);
+        int k=system("fc std.out test.out");
+        if (k) break;
+        printf("AC\n");
+    }
+    return 0;
+}
