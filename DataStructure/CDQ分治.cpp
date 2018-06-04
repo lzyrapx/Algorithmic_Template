27dc03c9a446f48b6f76ba3670fddbb597e34e59
LzyRapx <LzyRapx@gmail.com>
2018-06-04 14:54:04 +0800

    CDQ分治

--- DataStructure/CDQ分治.cpp
+++ DataStructure/CDQ分治.cpp
@@ -1,88 +1,123 @@
 
 /*
 CDQ分治解决高维数点问题
-
+以经典的陌上花开例题为例~
 */
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int maxn = 200010;
+const int mod = 1e9 + 7;
 
-const int N = 401000;
+// https://www.lydsy.com/JudgeOnline/problem.php?id=3262
+// 一朵花的级别是它拥有的美丽能超过的花的数量
+// 统计评级为 0...n-1的每级花的数量
+// solution :  O(n*logn*logn)
 
-int n,m;
+int n,k;
 
-typedef struct seg{
-	int x,y,z,kind,id;
-}seg;
+int s[maxn];
+int ans[maxn];
 
-seg v[N],quryx[N];
+struct Query
+{
+  int x,y,z;
+  int ans,cnt;
+}a[maxn],q[maxn];
 
-int c[N],maxv,ans[N];
-
-bool cmpx(seg a,seg b){
-	return a.x < b.x || (a.x == b.x && a.kind < b.kind);
+inline int lowbit(int x)
+{
+  return x&(-x);
+}
+void update(int pos,int val)
+{
+  while(pos <= k) {
+    s[pos] += val;
+    pos += lowbit(pos);
+  }
+}
+int query(int pos)
+{
+  int res = 0;
+  while(pos > 0) {
+    res += s[pos];
+    pos -= lowbit(pos);
+  }
+  return res;
 }
 
-bool cmpy(seg a,seg b){
-	return a.y < b.y || (a.y == b.y && a.kind < b.kind);
+bool cmpy(Query a, Query b)
+{
+  if(a.y == b.y) return a.z < b.z;
+  return a.y < b.y;
 }
 
-int lowbit(int x){return x & (-x);}
+void cdq(int l, int r)
+{
+  if(l >= r) return;
+  int mid = (l + r) >> 1;
+  cdq(l, mid);
+  cdq(mid + 1, r);
+  // 每次把平分的两个数组进行排序
+  sort(q + l, q  +  mid + 1, cmpy);
+  sort(q + mid + 1, q + r + 1, cmpy);
 
-int add(int x,int v){
-	for (;x <= maxv;x += lowbit(x))
-		c[x] += v;
+  // 然后扫一遍.
+  // 因为排过序.
+  // 所以如果对于[low,mid]里的 i 对[mid+1,r]里的 j 有贡献的话，
+  // 那么它对 j+1 也是有贡献的
+
+  // 单独处理前一部分和处理前一部分对后一部分的影响，就是 CDQ 分治
+  int low = l, high = mid + 1;
+  for(high = mid + 1; high <= r; high ++)
+  {
+    while(low <= mid && q[low].y <= q[high].y)
+    {
+      update(q[low].z,q[low].cnt);
+      low++;
+    }
+    // 统计树状数组中所有下标小于等于 q.z 的值之和累加到 q.ans中
+    q[high].ans += query(q[high].z);
+  }
+  // 最后要去掉[l,low-1]的贡献, 相当于撤销操作
+  // std::cout << "low = " << low << '\n';
+  for(int i = l; i < low; i++) {
+    update(q[i].z ,-q[i].cnt);
+  }
 }
-
-int sum(int x){
-	int ans = 0;
-	for (;x;x -= lowbit(x))
-		ans += c[x];
-	return ans;
+bool cmpx(Query a, Query b)
+{
+   if(a.x == b.x && a.y == b.y) return a.z < b.z;
+   else if(a.x == b.x) return a.y < b.y;
+   return a.x < b.x;
 }
-
-void xsolve(int l,int r){
-	if (l >= r) return;
-	for (int i = l;i <= r;i++){
-		if (quryx[i].kind == 0) add(quryx[i].z,1);
-		else ans[quryx[i].id] += sum(quryx[i].z);
-	}
-	for (int i = l;i <= r;i++){
-		if (quryx[i].kind == 0) add(quryx[i].z,-1);
-	}
-}
-
-void solve(int l,int r){
-	if (l >= r) return;
-	int mid = (l + r) >> 1;
-	solve(l,mid);
-	solve(mid+1,r);
-	int num = 0;
-	for (int i = l;i <= mid;i++)
-		if (v[i].kind == 0) quryx[++num] = v[i];
-	for (int i = mid+1;i <= r;i++)
-		if (v[i].kind) quryx[++num] = v[i];
-	sort(quryx+1,quryx+num+1,cmpy);
-	xsolve(1,num);
-}
-
-
-int main(){
-	int T; cin >> T;
-	while(T--){
-		scanf("%d",&n);
-		memset(c,0,sizeof(c));
-		memset(ans,0,sizeof(ans));
-		memset(v,0,sizeof(v));
-		for (int i = 1;i <= n;i++){
-			scanf("%d%d%d",&v[i].x,&v[i].y,&v[i].z);
-			v[n + i] = v[i];
-			v[n + i].kind = 1;
-			v[i].id = v[i + n].id = i;
-			maxv = max(maxv,v[i].x);
-			maxv = max(maxv,v[i].y);
-			maxv = max(maxv,v[i].z);
-		}
-		sort(v+1,v+2*n+1,cmpx);
-		solve(1,2*n);
-		for (int i = 1;i <= n;i++)
-            printf("%d\n",ans[i] - 1);
-	}
+int main(int argc, char const *argv[]) {
+  // freopen("in.txt","r",stdin);
+  std::cin >> n >> k;
+  for(int i = 1; i <= n; i++) {
+    std::cin >> a[i].x >> a[i].y >> a[i].z;
+  }
+  sort(a + 1, a + n + 1, cmpx); // 排序去掉其中一维
+  int cnt = 0;
+  int id = 0;
+  for(int i = 1; i <= n; i++) {
+    cnt ++; // 对于完全相同的点可以先合并起来, 后面统计在加起来=(q[i].cnt - 1)
+    if(a[i].x != a[i+1].x || a[i].y != a[i+1].y || a[i].z != a[i+1].z)
+    {
+      q[++id] = a[i];
+      q[id].cnt = cnt;
+      cnt = 0;
+    }
+  }
+  // std::cout << "id = " << id << '\n';
+  cdq(1,id);
+  // 最后统计评级为0...n-1的每级花的数量，n-1最高，即其他花都不能比
+  for(int i = 1; i <= id; i++) {
+    // std::cout << "ans = " << q[i].ans << '\n';
+    ans[q[i].ans + (q[i].cnt - 1)] += q[i].cnt; // (q[i].cnt - 1) 表示被合并的，和 i 一样的其它的花，不算自己
+  }
+  for(int i = 0; i < n; i++) {
+    std::cout << ans[i] << '\n';
+  }
+  return 0;
 }
