6000f22159c74773ee3a9be329b17f7cf278494b
LzyRapx <1303377506@qq.com>
2020-05-16 14:22:23 +0800

    format trie

--- String/动态Trie.cpp
+++ String/动态Trie.cpp
@@ -1,67 +1,61 @@
+/*
+ * @Author: zhaoyang.liang
+ * @Github: https://github.com/LzyRapx
+ * @Date: 2019-06-13 00:02:05
+ */ 
 /*  Trie   */
 /*  动态建立,注意释放内存  */
-const int wide=26;  //每个节点的最大子节点数
+const int wide = 26;  //每个节点的最大子节点数
 
-struct Trie 
-{
-    struct Node
-    {
-        Node(){
-            memset(ch,0,sizeof(ch));
-            val=0;
-        }
-        Node *ch[26];
-        int val;
-    };
-    Node *head;
+struct Trie {
+  struct Node {
+    Node() {
+      memset(ch, 0, sizeof(ch));
+      val = 0;
+    }
+    Node *ch[26];
+    int val;
+  };
+  Node *head;
 
-    void init(){
-
-        head=new Node;
+  void init() { head = new Node; }
+  inline int idx(char c) { return c - 'a'; }
+  void insert(char *s, int v)  //插入, v 从 1 开始标号
+  {
+    int i, len = strlen(s), c;
+    Node *p = head, *q;
+    for (i = 0; i < len; i++) {
+      c = idx(s[i]);  //求对应编号
+      if (!p->ch[c]) {
+        q = new Node;
+        p->ch[c] = q;
+      }
+      p = p->ch[c];
     }
-    inline int idx(char c)
-    {
-        return c-'a';
+    p->val = v;
+  }
+  int find(char *s)  //查找
+  {
+    int i, len = strlen(s), c;
+    Node *p = head;
+    for (i = 0; i < len; i++) {
+      c = idx(s[i]);
+      if (!p->ch[c]) return 0;
+      p = p->ch[c];
     }
-    void insert(char *s,int v) //插入, v 从 1 开始标号
-    {   
-        int i,len=strlen(s),c;
-        Node *p=head,*q;
-        for(i = 0;i < len;i++)
-        {
-            c=idx(s[i]);   //求对应编号
-            if(!p->ch[c])
-            {
-                q = new Node;
-                p->ch[c]=q;
-            }
-            p = p->ch[c];
-        }
-        p->val = v;
+    for (i = 0; i < 26; i++) {
+      if (p->ch[i]) return 1;  // s串是模板串中某个串的prefix
     }
-    int find(char *s) //查找
-    {    
-        int i,len=strlen(s),c;
-        Node *p=head;
-        for(i = 0;i < len;i++)
-        {
-            c=idx(s[i]);
-            if(!p->ch[c]) return 0;
-            p=p->ch[c];
-        }
-        for(i=0;i<26;i++){
-            if(p->ch[i]) return 1;   //s串是模板串中某个串的prefix
-        }
-        return 2; //存在
+    return 2;  //存在
+  }
+  void free(Node *p)  //释放内存
+  {
+    int i;
+    for (i = 0; i < 26; i++) {
+      if (p->ch[i]) {
+        free(p->ch[i]);
+      }
     }
-    void free(Node *p) //释放内存
-    {   
-        int i;
-        for(i = 0;i < 26;i++){
-            if(p->ch[i]){
-            	free(p->ch[i]);
-            }
-        }
-        delete p;
-    }
-}trie;
+    delete p;
+  }
+} trie;
--- String/静态Trie.cpp
+++ String/静态Trie.cpp
@@ -1,52 +1,47 @@
 /*  Trie   */
-
 /*  静态化,注意内存开辟大小    */
+int wide = 26;        //每个节点的最大子节点数
+int ch[1 << 18][26];  //内存开辟尽量最大
+int val[1 << 18];
+int sz;
 
-const int wide=26;  //每个节点的最大子节点数
+void init() {
+  sz = 1;
+  memset(ch[0], 0, sizeof(ch[0]));
+}
 
-struct Trie 
+inline int idx(char c)  //字母映射
 {
-    int ch[1<<13][wide];    //内存开辟尽量最大
-    int val[1<<13];
-    int sz;
+  return c - 'a';
+}
 
-    void init()
-    {
-    	sz=1;
-    	memset(ch[0],0,sizeof(ch[0]));
+// v 从 1 开始标号,
+// 调用了insert(word[i],i)语句，如果给模板标号0的话相当于舍弃了这个模板串(v==0代表非单词结点)
+void insert(string s, int v)  //建立Trie树, v 从 1 开始标号
+{
+  int i, len = s.size(), c, u = 0;
+  for (i = 0; i < len; i++) {
+    c = idx(s[i]);
+    if (!ch[u][c]) {
+      memset(ch[sz], 0, sizeof(ch[sz]));
+      val[sz] = 0;
+      ch[u][c] = sz++;
     }
-    inline int idx(char c) //字母映射
-    {
-    	return c-'a';
-    }
-    // v 从 1 开始标号, 调用了insert(word[i],i)语句，如果给模板标号0的话相当于舍弃了这个模板串(v==0代表非单词结点)
-    void insert(char *s,int v) //建立Trie树, v 从 1 开始标号
-    {     
-        int i,len=strlen(s),c,u=0;
-        for(i = 0;i < len;i++)
-        {
-            c=idx(s[i]);
-            if(!ch[u][c])
-            {
-            	memset(ch[sz],0,sizeof(ch[sz]));
-                val[sz]=0;
-                ch[u][c]=sz++;
-            }
-            u=ch[u][c];
-        }
-        val[u]=v;
-    }
-    int find(char *s)  //查找
-    { 
-        int i,len=strlen(s),c,u=0;
-        for(i = 0;i < len;i++){
-            c=idx(s[i]);
-            if(!ch[u][c])return 0;
-            u=ch[u][c];
-        }
-        for(i=0;i<wide;i++) {
-            if(ch[u][c])return 1;   //s串是模板串中某个串的prefix
-        }
-        return 2;  //存在
-    }
-}trie;
+    u = ch[u][c];
+  }
+  val[u] = v;
+}
+
+int find(string s)  //查找
+{
+  int i, len = s.size(), c, u = 0;
+  for (i = 0; i < len; i++) {
+    c = idx(s[i]);
+    if (!ch[u][c]) return 0;
+    u = ch[u][c];
+  }
+  for (i = 0; i < wide; i++) {
+    if (ch[u][c]) return 1;  // s串是模板串中某个串的prefix
+  }
+  return 2;  //存在
+}
